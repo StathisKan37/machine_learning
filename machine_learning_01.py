350ffbec979561a416f1db137e844b6ee67bcc6f
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-04-28 16:03:14 -0400

    Update machine_learning_01.py

--- machine_learning_01.py
+++ machine_learning_01.py
@@ -1,24 +1,24 @@
 import numpy
 from scipy import stats
 
-array_1 = [99,86,87,88,111,86,103,87,94,78,77,85,86]
-print(f'Array: {array_1}')
+test_array = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+print(f'Array: {test_array}')
 
 # Mean: (x1 + x2 + x3 + ... + xn) / n
-print(f'Mean value: {numpy.mean(array_1)}')
+print(f'Mean value: {numpy.mean(test_array)}')
 
 # Median: The value in the middle of the sorted array
-print(f'Median value: {numpy.median(array_1)}')
+print(f'Median value: {numpy.median(test_array)}')
 # If there are two, divide the sum by two
 
 # Median: The that appears the most number of times
-print(stats.mode(array_1))
+print(stats.mode(test_array))
 
 # Standard deviation: Describes how spread out the values are
-print(f'Standard deviation: {numpy.std(array_1)}')
+print(f'Standard deviation: {numpy.std(test_array)}')
 
 # Percentiles: Describes the value that a given percent of the values are lower than
-print(f'Percentiles (75%): {numpy.percentile(array_1, 75)}')
+print(f'Percentiles (75%): {numpy.percentile(test_array, 75)}')
 
 # SOURCE:
 # https://www.w3schools.com/python/python_ml_mean_median_mode.asp
