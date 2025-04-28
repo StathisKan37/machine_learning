71b7c83f987d9ceb67cc3daf08263666fe2c7f8e
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-04-28 16:18:06 -0400

    Create ML_03_Normal_data.py

---
+++ ML_03_Normal_data.py
@@ -0,0 +1,12 @@
+import numpy
+import matplotlib.pyplot as plt
+
+# An array with 2500 random floats from 0 to 1
+test_array = numpy.random.normal(0, 1, 2500)
+
+# Histogram with 100 bars of the array
+plt.hist(test_array, 100)
+plt.show()
+
+# SOURCE:
+# https://www.w3schools.com/python/python_ml_normal_data_distribution.asp
