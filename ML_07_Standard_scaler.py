ff4a8c02bd6c5e96c2ba640768dffd8527646596
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-04-30 14:41:50 -0400

    Update ML_07_Standard_scaler.py

--- ML_07_Standard_scaler.py
+++ ML_07_Standard_scaler.py
@@ -1,12 +1,33 @@
 import pandas
 from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
-scale = StandardScaler()
 
-df = pandas.read_csv("data.csv")
+# Reading the .csv file 'car_data.csv'
+dataframe = pandas.read_csv('car_data.csv')
+print(dataframe)
 
-X = df[['Weight', 'Volume']]
+# StandardScaler(): returns a Scaler object for transforming data sets
+std_scaler = StandardScaler()
 
-scaledX = scale.fit_transform(X)
+# The independent and dependent values
+array_x = dataframe[['Weight', 'Volume']]
+array_y = dataframe['CO2']
 
-print(scaledX) 
+# Fitting the scaler to X and transform it
+scaled_x = std_scaler.fit_transform(array_x)
+
+# Creating a linear regression line
+reg_line = linear_model.LinearRegression()
+reg_line.fit(scaled_x, array_y)
+
+# Predicting the CO2 emission of a test car
+print('CO2 prediction:')
+print('Car weight: 2300kg')
+print('Car volume: 1300cm3')
+# Scaling the test input
+scaled_test_input = std_scaler.transform([[2300, 1300]])
+predicted_co2 = reg_line.predict(scaled_test_input)
+print(f'CO2 emissions: {predicted_co2[0]}')
+
+# SOURCE:
+# https://www.w3schools.com/python/python_ml_scale.asp
