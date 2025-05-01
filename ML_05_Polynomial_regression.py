ac340f4bcf15442f0ab1da4345cb7f5beb69aa60
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-05-01 17:01:39 +0300

    Update ML_05_Polynomial_regression.py

--- ML_05_Polynomial_regression.py
+++ ML_05_Polynomial_regression.py
@@ -7,7 +7,7 @@ array_x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
 array_y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
 
 # Making a polynomial model
-model_line = np.poly1d(np.polyfit(array_x, array_y, 3)) 
+model_object = np.poly1d(np.polyfit(array_x, array_y, 3)) 
 
 # Displaying the line
 # Starting at position 1, end at position 22
@@ -15,7 +15,7 @@ myline = np.linspace(1, 22, 100)
 
 # Displaying the result
 plt.scatter(array_x, array_y)
-plt.plot(myline, model_line(myline))
+plt.plot(myline, model_object(myline))
 plt.show() 
 
 # R Squared: How well the relationship between the values of the x- and y-axis is
