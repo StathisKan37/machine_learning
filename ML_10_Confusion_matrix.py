6fb4e39109976d46155cf1160cb012a7e9fbd4e4
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-06-08 18:39:52 +0300

    Update ML_10_Confusion_matrix.py

--- ML_10_Confusion_matrix.py
+++ ML_10_Confusion_matrix.py
@@ -13,11 +13,11 @@ confusion_matrix = metrics.confusion_matrix(val_act, val_pred)
 cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
 
 # Printing the proportion of correct predictions
-print(f'Accuracy: {metrics.accuracy_score(actual, predicted)}')
-print(f'Precision: {metrics.precision_score(actual, predicted)}')
-print(f'Sensitivity recall: {metrics.recall_score(actual, predicted)}')
-print(f'Specificity: {metrics.recall_score(actual, predicted, pos_label=0)}')
-print(f'F1_score: {metrics.f1_score(actual, predicted)}')
+print(f'Accuracy: {metrics.accuracy_score(val_act, val_pred)}')
+print(f'Precision: {metrics.precision_score(val_act, val_pred)}')
+print(f'Sensitivity recall: {metrics.recall_score(val_act, val_pred)}')
+print(f'Specificity: {metrics.recall_score(val_act, val_pred, pos_label=0)}')
+print(f'F1_score: {metrics.f1_score(val_act, val_pred)}')
 
 # Displaying the plot
 cm_display.plot()
