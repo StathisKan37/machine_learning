3913369f79dec271c287011bc5419e9b2b71ddc8
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-06-10 18:38:36 +0300

    Update ML_04_Regression_line.py

--- ML_04_Regression_line.py
+++ ML_04_Regression_line.py
@@ -3,15 +3,15 @@ import numpy
 from scipy import stats
 
 # This function returns the regression line
-def regress_line(array_x):
-  return array_x * slope + intercept
+def regress_line(X_array):
+  return X_array * slope + intercept
 
 # Two arrays to represent X axis and Y axis
-array_x = numpy.sort(numpy.random.randint(0, 99, 30)) - numpy.random.randint(0,10,30)
-array_y = numpy.sort(numpy.random.randint(0, 99, 30))
+X_array = numpy.sort(numpy.random.randint(0, 99, 30)) - numpy.random.randint(0,10,30)
+y_array = numpy.sort(numpy.random.randint(0, 99, 30))
 
 # Linear Regression Calculation
-slope, intercept, r, pvalue, std_err = stats.linregress(array_x, array_y)
+slope, intercept, r, pvalue, std_err = stats.linregress(X_array, y_array)
 #     slope: How much y increases for a unit increase in x
 # intercept: The value of y when x = 0
 #         r: How strong and linear the relationship is (-1 to 1)
@@ -19,11 +19,11 @@ slope, intercept, r, pvalue, std_err = stats.linregress(array_x, array_y)
 #   std_err: The standard error of the estimated slope.
 
 # Creating the regression line
-model_line = list(map(regress_line, array_x))
+model_line = list(map(regress_line, X_array))
 
 # Displaying the result
-plt.scatter(array_x, array_y)
-plt.plot(model_line, array_x)
+plt.scatter(X_array, y_array)
+plt.plot(model_line, X_array)
 plt.show()
 
 # SOURCE:
